a27f87c2a265ee3f03922468766eb5d5e71feaf1
Arjun <86774036+arjun1607@users.noreply.github.com>
2024-10-26 07:54:37 +0000

    class

---
+++ 10_classes_and_oops/06_classes.js
@@ -0,0 +1,37 @@
+// ES6
+
+class User {
+    constructor(username, email, password){
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    // these are called method inside class, not function
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai = new User('chai', 'chai@google.com', 123)
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+
+// behind the scene
+
+function User2(username, email, password) {
+    this.username = username
+    this.email = email
+    this.password = password
+}
+
+User2.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+const tea = new User2("tea", "tea@gmail.com", "221")
+console.log(tea.encryptPassword());
