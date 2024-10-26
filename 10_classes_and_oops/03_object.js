dc9532d44cc7c079e90d8f81311e4a222f06ccf6
Arjun <86774036+arjun1607@users.noreply.github.com>
2024-10-26 06:26:33 +0000

    proto

--- 10_classes_and_oops/03_object.js
+++ 10_classes_and_oops/03_object.js
@@ -1,20 +1,53 @@
-function multipleBy5(num){
-    return num*5
+function multipleBy5(num) {
+    return num * 5
 }
 multipleBy5.power = 2
 console.log(multipleBy5(5));          // 25
 console.log(multipleBy5.power);       // 2
 console.log(multipleBy5.prototype);   // {}   
-console.log(multipleBy5.prototype.constructor)     //[Function: multipleBy5] { power: 2 }
-// When you access the prototype property of a function, you're essentially looking at a plain object. 
-// This object, by default, contains a single property: constructor, which points back to the function itself.
 // You can add properties and methods to this prototype object to be inherited by instances of the function.
+
 
 // The prototype property is associated with functions, not directly with objects.
 const arr = {}
 console.log(arr.prototype);  // undefined
 // When you create an array, it inherits properties and methods from the Array.prototype. 
 // However, you don't directly access the prototype property to use these methods.
+
+
+
+
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.greet = function () {
+    console.log("Hello, my name is " + this.name);
+};
+
+const person1 = new Person("Alice");
+person1.greet(); // Output: Hello, my name is Alice
+console.log(Person.prototype)   // { greet: [Function (anonymous)] }
+console.log(person1.prototype)  // undefined    becoz its object instance
+
+
+
+
+
+function createUser(username, score) {
+    this.username = username
+    this.score = score
+    let a = 10
+}
+createUser.prototype.increment = function () {
+    this.score++
+}
+createUser.prototype.printMe = function(){
+    console.log(this.username)
+}
+const chai = new createUser('chai', 25)
+const tea = new createUser('tea', 250)
+chai.printMe()  // chai
+console.log(chai)  // createUser { username: 'chai', score: 25 }      it does not have 'a'
 
 
 
@@ -27,7 +60,8 @@ console.log(arr.prototype);  // undefined
 
 Here's what happens behind the scenes when the new keyword is used:
 
-A new object is created: The new keyword initiates the creation of a new JavaScript object.
+A new object is created: The new keyword initiates the creation of a new JavaScript object. Conctructor function 
+is reference for that new object. 
 
 A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. 
 This means that it has access to properties and methods defined on the constructor's prototype.
