32da5f3921f26a08ad75a0eedbf2a137557fd08c
Arjun <86774036+arjun1607@users.noreply.github.com>
2024-10-26 02:29:42 +0000

    fetch

--- 09_advance/03_promises.js
+++ 09_advance/03_promises.js
@@ -1,3 +1,10 @@
+// fetch() method of the Window interface starts the process of fetching a resource from the network,
+// returning a promise that is fulfilled once the response is available.
+// fetch() promise does not reject if the server responds with HTTP status codes that indicate errors (404, 504, etc.)
+// only rejects when the request fails, for example, because of a badly-formed request URL or a network error. 
+
+// for fetch(), microtask queue / priority queue is made. That's why it will run first than other async funcs like setTimeout
+
 async function getAllUsers() {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
